eac3c61ee1721401066da54dad7e898cd0458369
jonoryan42 <20101393@mail.wit.ie>
2025-11-11 04:32:17 +0000

    Completed node lab 1!

--- node_lab1/greeting.js
+++ node_lab1/greeting.js
@@ -1,6 +1,9 @@
 const greeting = {
     en: "Hello World!",
-    fr: "Bonjour!"
+    fr: "Bonjour!",
+    ar: " مرحبا بالعالم",
+    ga: "Dia Duit!",
+    zh: "你好，世界！",
 };
 
 export default greeting;
